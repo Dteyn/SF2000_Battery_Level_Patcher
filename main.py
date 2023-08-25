08f253c9908b3dc272bc205a47935a3b176b5d53
Dteyn <dteyn.social@gmail.com>
2023-08-25 09:28:43 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,96 @@
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+# Change battery values here
+BATTERY_VALUES = [
+    (0x3564ec, 0xBF),  # Battery level 3, stock value 0xBF
+    (0x3564f4, 0xB3),  # Battery level 2, stock value 0xB7
+    (0x35658c, 0xA5),  # Battery level 1, stock value 0xAF
+    (0x356594, 0x96),  # Battery level 0, stock value 0xA9
+    (0x3565b0, 0x6E)   # Battery level -1, stock value 0xA1
+]
+
+# Stock values for sanity check
+STOCK_VALUES = {
+    0x3564ec: 0xBF,
+    0x3564f4: 0xB7,
+    0x35658c: 0xAF,
+    0x356594: 0xA9,
+    0x3565b0: 0xA1
+}
+
+
+def calculate_crc32(data):
+    """
+    Calculate the CRC32 value for the given data.
+    """
+    tab_crc32 = [(i << 24) & 0xFFFFFFFF for i in range(256)]
+    for i in range(256):
+        c = tab_crc32[i]
+        for _ in range(8):
+            c = (c << 1) ^ 0x4c11db7 if (c & (1 << 31)) else c << 1
+            c &= 0xFFFFFFFF
+        tab_crc32[i] = c
+
+    c = ~0 & 0xFFFFFFFF
+    for i in range(512, len(data)):
+        c = (c << 8) ^ tab_crc32[((c >> 24) ^ data[i]) & 0xFF]
+        c &= 0xFFFFFFFF
+
+    return c
+
+
+def sanity_check(bisrv_data):
+    """
+    Check if the firmware matches the expected "08.03" version.
+    """
+    for addr, expected_value in STOCK_VALUES.items():
+        if bisrv_data[addr] != expected_value:
+            logging.error("The firmware does not match the expected '08.03' version at offset %X. Please check the offsets.", addr)
+            return False
+    return True
+
+
+def patch_firmware(filename):
+    """
+    Patch the firmware file with new battery values and update its CRC32.
+    """
+    try:
+        with open(filename, 'rb') as f:
+            bisrv_data = bytearray(f.read())
+        logging.info("File '%s' opened successfully.", filename)
+
+        # Perform sanity check
+        if not sanity_check(bisrv_data):
+            return
+
+        # Patch the battery values
+        for addr, value in BATTERY_VALUES:
+            bisrv_data[addr] = value
+        logging.info("File patched with new battery values.")
+
+        # Calculate new CRC32
+        crc = calculate_crc32(bisrv_data)
+        logging.info("New CRC32 value calculated: %X", crc)
+
+        # Update CRC32 in the bisrv_data
+        bisrv_data[0x18c] = crc & 0xFF
+        bisrv_data[0x18d] = (crc >> 8) & 0xFF
+        bisrv_data[0x18e] = (crc >> 16) & 0xFF
+        bisrv_data[0x18f] = (crc >> 24) & 0xFF
+
+        # Write the patched data back to the file
+        with open(filename, 'wb') as f:
+            f.write(bisrv_data)
+        logging.info("Patched data written back to '%s'.", filename)
+
+    except FileNotFoundError:
+        logging.error("File '%s' not found.", filename)
+    except Exception as e:
+        logging.error("An error occurred: %s", str(e))
+
+
+if __name__ == "__main__":
+    patch_firmware('bisrv.asd')
